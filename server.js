1733bd69ada7b2828fcab29cd85a2d1393d8de7c
asnaniseema <82732613+asnaniseema@users.noreply.github.com>
2021-05-09 12:46:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ let app = http.createServer(
 	function(request, response){
 		request.addListener('end',
 			function(){
-				file.server(request.response);
+				file.serve(request.response);
 			}
 		
 		
